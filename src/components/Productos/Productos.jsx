91a49d21f9acdf5f2c0f8d70a0d877a9d3903e8d
Santiago Corbanini <santiagocorbanini@gmail.com>
2024-11-11 21:10:33 -0300

    agrego estilos a las cards

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -9,134 +9,131 @@ import imgFertilizantes from "./../../assets/secciones/fertilizantes.png";
 import imgBioestimulantes from "./../../assets/secciones/bioestimulantes.png";
 
 function Productos() {
-  const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(null);
 
-  const filteredProducts = selectedCategory
-    ? productData.filter((product) => product.titulo === selectedCategory)
-    : productData;
+    const filteredProducts = selectedCategory
+        ? productData.filter((product) => product.titulo === selectedCategory)
+        : productData;
 
-  return (
-    <Container className="page-section justify-content-center">
-      <Container className="category-selector d-flex justify-content-around mb-4">
-        <Row>
-          <Col
-            xs={12}
-            md={4}
-            className="d-flex justify-content-center mb-3 mb-md-0"
-          >
-            <img
-              src={imgAdyuvantes}
-              alt="Adyuvantes"
-              className={`category-image ${
-                selectedCategory === "adyuvantes" ? "active" : "inactive"
-              }`}
-              onClick={() => setSelectedCategory("adyuvantes")}
-            />
-          </Col>
-          <Col
-            xs={12}
-            md={4}
-            className="d-flex justify-content-center mb-3 mb-md-0"
-          >
-            <img
-              src={imgFertilizantes}
-              alt="Fertilizantes"
-              className={`category-image ${
-                selectedCategory === "fertilizantes" ? "active" : "inactive"
-              }`}
-              onClick={() => setSelectedCategory("fertilizantes")}
-            />
-          </Col>
-          <Col xs={12} md={4} className="d-flex justify-content-center">
-            <img
-              src={imgBioestimulantes}
-              alt="Bioestimulantes"
-              className={`category-image ${
-                selectedCategory === "bioestimulantes" ? "active" : "inactive"
-              }`}
-              onClick={() => setSelectedCategory("bioestimulantes")}
-            />
-          </Col>
-        </Row>
-      </Container>
-
-      <Row>
-        {filteredProducts.map((product) => (
-          <Col md={6} key={product.id} className="mb-4">
-            <Card>
-              <Card.Img variant="top" src={product.logo} />
-              <Card.Body className="px-10">
-                <Card.Text>{product.descripcion}</Card.Text>
-                <div className="mt-3">
-                  <Row>
-                    <Col md={8} className="pe-0">
-                      <ul className="benefits-list">
-                        {product.beneficios.map((beneficio, index) => (
-                          <li className="pb-1" key={index}>
-                            {" "}
-                            {beneficio}
-                          </li>
-                        ))}
-                      </ul>
+    return (
+        <Container className="page-section main-section">
+            <Container className="category-selector d-flex justify-content-around mb-4">
+                <Row>
+                    <Col
+                        xs={12}
+                        md={4}
+                        className="d-flex justify-content-center mb-3 mb-md-0"
+                    >
+                        <img
+                            src={imgAdyuvantes}
+                            alt="Adyuvantes"
+                            className={`category-image ${selectedCategory === "adyuvantes" ? "active" : "inactive"
+                                }`}
+                            onClick={() => setSelectedCategory("adyuvantes")}
+                        />
                     </Col>
-                    <Col md={4} className="p-0">
-                      <ul className="hojas-list">
-                        <Button href={product.marbetes} variant="link">
-                          <img
-                            src={logo}
-                            alt="icono"
-                            className="icono-link me-2"
-                          />
-                          Ver marbete
-                        </Button>
-                        <Button href={product.hojaSeguridad} variant="link">
-                          <img
-                            src={logo}
-                            alt="icono"
-                            className="icono-link me-2"
-                          />
-                          Ver reglas de seguridad
-                        </Button>
-                        {product.imagen && (
-                          <Button href={product.imagen} variant="link">
-                            <img
-                              src={logo}
-                              alt="icono"
-                              className="icono-link me-2"
-                            />
-                            Ver imagen
-                          </Button>
-                        )}
-                      </ul>
+                    <Col
+                        xs={12}
+                        md={4}
+                        className="d-flex justify-content-center mb-3 mb-md-0"
+                    >
+                        <img
+                            src={imgFertilizantes}
+                            alt="Fertilizantes"
+                            className={`category-image ${selectedCategory === "fertilizantes" ? "active" : "inactive"
+                                }`}
+                            onClick={() => setSelectedCategory("fertilizantes")}
+                        />
                     </Col>
-                  </Row>
-                </div>
-                <Card.Title className="mb-0 custom-margin-left">
-                  Presentación:
-                </Card.Title>
-                <Card.Text className="custom-margin-left">
-                  {" "}
-                  {product.presentacion}
-                </Card.Text>
-                <Card.Title className="mb-0 custom-margin-left">
-                  Dosis:
-                </Card.Title>
-                <Card.Text className="custom-margin-left">
-                  {product.dosis}
-                </Card.Text>
-                <Card.Title className="caracteristics p-1">
-                  Características:
-                </Card.Title>
-                <Card.Text className="custom-margin-left">
-                  {product.caracteristicas}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
+                    <Col xs={12} md={4} className="d-flex justify-content-center">
+                        <img
+                            src={imgBioestimulantes}
+                            alt="Bioestimulantes"
+                            className={`category-image ${selectedCategory === "bioestimulantes" ? "active" : "inactive"
+                                }`}
+                            onClick={() => setSelectedCategory("bioestimulantes")}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+
+            <Row>
+                {filteredProducts.map((product) => (
+                    <Col md={6} key={product.id} className="mb-4">
+                        <Card>
+                            <Card.Img variant="top" src={product.logo} />
+                            <Card.Body className="px-10">
+                                <Card.Text>{product.descripcion}</Card.Text>
+                                <div className="mt-3">
+                                    <Row>
+                                        <Col md={8} className="pe-0">
+                                            <ul className="benefits-list">
+                                                {product.beneficios.map((beneficio, index) => (
+                                                    <li className="pb-1" key={index}>
+                                                        {" "}
+                                                        {beneficio}
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </Col>
+                                        <Col md={4} className="p-0">
+                                            <ul className="hojas-list">
+                                                <Button href={product.marbetes} variant="link">
+                                                    <img
+                                                        src={logo}
+                                                        alt="icono"
+                                                        className="icono-link me-2"
+                                                    />
+                                                    Ver marbete
+                                                </Button>
+                                                <Button href={product.hojaSeguridad} variant="link">
+                                                    <img
+                                                        src={logo}
+                                                        alt="icono"
+                                                        className="icono-link me-2"
+                                                    />
+                                                    Ver reglas de seguridad
+                                                </Button>
+                                                {product.imagen && (
+                                                    <Button href={product.imagen} variant="link">
+                                                        <img
+                                                            src={logo}
+                                                            alt="icono"
+                                                            className="icono-link me-2"
+                                                        />
+                                                        Ver imagen
+                                                    </Button>
+                                                )}
+                                            </ul>
+                                        </Col>
+                                    </Row>
+                                </div>
+                                <Card.Title className="mb-0 custom-margin-left">
+                                    Presentación:
+                                </Card.Title>
+                                <Card.Text className="custom-margin-left">
+                                    {" "}
+                                    {product.presentacion}
+                                </Card.Text>
+                                <Card.Title className="mb-0 custom-margin-left">
+                                    Dosis:
+                                </Card.Title>
+                                <Card.Text className="custom-margin-left">
+                                    {product.dosis}
+                                </Card.Text>
+                                <Card.Title className="caracteristics p-1">
+                                    Características:
+                                </Card.Title>
+                                <Card.Text className="custom-margin-left">
+                                    {product.caracteristicas}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    );
 }
 
 export default Productos;
