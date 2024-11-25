4b3b4c63afa926c0a5cbc28b11d153d8bda299b8
Franco Vanney <108736068+francovanney@users.noreply.github.com>
2024-11-25 16:37:39 -0300

    Fixes Contacto

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,8 +1,9 @@
-import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import { Container, Form, Button, Row, Col, Image } from "react-bootstrap";
 import { Formik, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import Logo from "../../assets/icono_links.jpg";
 import "./Contacto.scss";
+import ubicacion from "../../assets/ubicacion.jpg";
 
 const Contacto = () => {
   const validationSchema = Yup.object({
@@ -16,7 +17,7 @@ const Contacto = () => {
 
   return (
     <Container>
-      <h3>Contacto</h3>
+      <h3 className="text-white">Contacto</h3>
       <Formik
         initialValues={{
           nombre: "",
@@ -33,111 +34,117 @@ const Contacto = () => {
         }}
       >
         {({ handleSubmit, isSubmitting }) => (
-          <Form onSubmit={handleSubmit}>
-            <Row>
-              <Col md={6}>
-                <Form.Group controlId="formNombre" className="my-3">
-                  <Field
-                    name="nombre"
-                    type="text"
-                    as={Form.Control}
-                    placeholder="Nombre"
-                  />
-                  <ErrorMessage
-                    name="nombre"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="formEmail" className="my-3">
-                  <Field
-                    name="email"
-                    type="email"
-                    as={Form.Control}
-                    placeholder="Email"
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="formTelefono" className="my-3">
-                  <Field
-                    name="telefono"
-                    type="text"
-                    as={Form.Control}
-                    placeholder="Teléfono"
-                  />
-                  <ErrorMessage
-                    name="telefono"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="formCiudadPais" className="my-3">
-                  <Field
-                    name="ciudadPais"
-                    type="text"
-                    as={Form.Control}
-                    placeholder="Ciudad/País"
-                  />
-                  <ErrorMessage
-                    name="ciudadPais"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-
-                <Form.Group controlId="formEmpresa" className="my-3">
-                  <Field
-                    name="empresa"
-                    type="text"
-                    as={Form.Control}
-                    placeholder="Empresa"
-                  />
-                  <ErrorMessage
-                    name="empresa"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-              </Col>
-
-              <Col md={6}>
-                <Form.Group controlId="formMensaje">
-                  <Field
-                    name="mensaje"
-                    as="textarea"
-                    rows={10}
-                    className="form-control"
-                    placeholder="Mensaje"
-                  />
-                  <ErrorMessage
-                    name="mensaje"
-                    component="div"
-                    className="text-danger"
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-
-            <Container className="d-flex justify-content-end mb-2 mt-2">
-              <Button onClick={handleSubmit}>
-                <img
-                  src={Logo}
-                  alt="Enviar"
-                  style={{
-                    cursor: "pointer",
-                  }}
-                />
-                Enviar
-              </Button>
+          <>
+            <Container className="d-flex justify-content-center mb-5 mt-4">
+              <Image src={ubicacion} alt="Ubicación" className="img-fluid" />
             </Container>
-          </Form>
+
+            <Form onSubmit={handleSubmit} className="mx-4 mb-5">
+              <Row>
+                <Col md={6}>
+                  <Form.Group controlId="formNombre" className="my-3">
+                    <Field
+                      name="nombre"
+                      type="text"
+                      as={Form.Control}
+                      placeholder="Nombre"
+                    />
+                    <ErrorMessage
+                      name="nombre"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formEmail" className="my-3">
+                    <Field
+                      name="email"
+                      type="email"
+                      as={Form.Control}
+                      placeholder="Email"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formTelefono" className="my-3">
+                    <Field
+                      name="telefono"
+                      type="text"
+                      as={Form.Control}
+                      placeholder="Teléfono"
+                    />
+                    <ErrorMessage
+                      name="telefono"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formCiudadPais" className="my-3">
+                    <Field
+                      name="ciudadPais"
+                      type="text"
+                      as={Form.Control}
+                      placeholder="Ciudad/País"
+                    />
+                    <ErrorMessage
+                      name="ciudadPais"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formEmpresa" className="my-3">
+                    <Field
+                      name="empresa"
+                      type="text"
+                      as={Form.Control}
+                      placeholder="Empresa"
+                    />
+                    <ErrorMessage
+                      name="empresa"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </Form.Group>
+                </Col>
+
+                <Col md={6}>
+                  <Form.Group controlId="formMensaje">
+                    <Field
+                      name="mensaje"
+                      as="textarea"
+                      rows={10}
+                      className="form-control"
+                      placeholder="Mensaje"
+                    />
+                    <ErrorMessage
+                      name="mensaje"
+                      component="div"
+                      className="text-danger"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+
+              <Container className="d-flex justify-content-end mb-2 mt-2">
+                <Button onClick={handleSubmit}>
+                  <img
+                    src={Logo}
+                    alt="Enviar"
+                    style={{
+                      cursor: "pointer",
+                    }}
+                  />
+                  Enviar
+                </Button>
+              </Container>
+            </Form>
+          </>
         )}
       </Formik>
     </Container>
