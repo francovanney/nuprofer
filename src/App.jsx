53bb0987c5a2d5e1a3ae3fdec142d7497f90cf1d
Franco Vanney <108736068+francovanney@users.noreply.github.com>
2024-12-20 10:25:23 -0300

    Fix route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 // App.js
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Productos from "./components/Productos/Productos";
 import Footer from "./components/Footer/Footer";
 import Contacto from "./components/Contacto/Contacto"; // Importa el componente Contacto
 
 function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Header />
-                <Routes>
-                    <Route>
-                        <Route path="/nuprofer" element={<Productos />} />
-                        <Route path="/contacto" element={<Contacto />} />
-                    </Route>
-                </Routes>
-                <Footer />
-            </Router>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/nuprofer" />} />
+          <Route path="/nuprofer" element={<Productos />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
